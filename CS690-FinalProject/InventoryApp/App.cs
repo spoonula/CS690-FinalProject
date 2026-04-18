b32a8857857301f94c2f4b05ba6fade13a8db2e4
spoonula <mowersch@gmail.com>
2026-04-18 16:33:03 +0000

    main menu

--- CS690-FinalProject/InventoryApp/App.cs
+++ CS690-FinalProject/InventoryApp/App.cs
@@ -8,4 +8,15 @@ class App
     {
         AnsiConsole.MarkupLine("[bold]Hello, World![/]");
     }
+
+    void MainMenu()
+    {
+        var domain = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("=== Main Menu ===")
+                .AddChoices("Items", "Locations", "Borrowers", "Reports", "Exit")
+        );
+
+        AnsiConsole.WriteLine(domain)
+    }
 }
