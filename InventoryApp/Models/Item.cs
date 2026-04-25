ee815d5516c186d9297be12502fe88ef5e82811c
spoonula <mowersch@gmail.com>
2026-04-25 11:31:34 +0000

    removed comments

--- InventoryApp/Models/Item.cs
+++ InventoryApp/Models/Item.cs
@@ -7,9 +7,5 @@ namespace InventoryApp.Models
         public string Description { get; set; } = string.Empty;
         public decimal EstimatedValue { get; set; }
         public Guid? LocationId { get; set; }
-        // TODO: Implement
-        
-        // public bool IsLoaned { get; set; }
-        // public int? BorrowerId { get; set; }
     }
 }
